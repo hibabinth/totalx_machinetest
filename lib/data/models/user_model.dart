afb340523484c8c277058b80c78e4033dbeb9571
hibabinth <hibabinth916@gmail.com>
2026-04-24 09:42:19 -0400

    Create user data model

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,40 @@
+class UserModel {
+  final String id;
+  final String name;
+  final String phone;
+  final int age;
+  final String imageUrl;
+  final DateTime createdAt;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.phone,
+    required this.age,
+    required this.imageUrl,
+    required this.createdAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'phone': phone,
+      'age': age,
+      'imageUrl': imageUrl,
+      'createdAt': createdAt,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'] ?? '',
+      name: map['name'] ?? '',
+      phone: map['phone'] ?? '',
+      age: map['age'] ?? 0,
+      imageUrl: map['imageUrl'] ?? '',
+      createdAt:
+          DateTime.tryParse(map['createdAt'].toString()) ?? DateTime.now(),
+    );
+  }
+}
