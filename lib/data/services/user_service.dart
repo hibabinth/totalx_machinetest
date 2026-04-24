68147aa6c884d91e656eab2d42f66fc2c1ffb561
hibabinth <hibabinth916@gmail.com>
2026-04-24 09:47:04 -0400

    Setup Firestore user service

---
+++ lib/data/services/user_service.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/user_model.dart';
+
+class UserService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> addUser(UserModel user) async {
+    await _firestore.collection('users').doc(user.id).set(user.toMap());
+  }
+
+  Stream<List<UserModel>> getUsers() {
+    return _firestore
+        .collection('users')
+        .orderBy('createdAt', descending: true)
+        .snapshots()
+        .map(
+          (snapshot) => snapshot.docs
+              .map((doc) => UserModel.fromMap(doc.data()))
+              .toList(),
+        );
+  }
+}
