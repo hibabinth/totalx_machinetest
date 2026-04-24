2c79ef6ec3ae67fdd10700ef2e9bcb3e696349e6
hibabinth <hibabinth916@gmail.com>
2026-04-24 09:48:37 -0400

    Add Firebase Storage image upload service

--- lib/data/services/user_service.dart
+++ lib/data/services/user_service.dart
@@ -1,8 +1,23 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
 import '../models/user_model.dart';
 
 class UserService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  Future<String> uploadUserImage(File imageFile) async {
+    final String fileName = DateTime.now().millisecondsSinceEpoch.toString();
+
+    final Reference ref = _storage.ref().child('user_images/$fileName.jpg');
+
+    await ref.putFile(imageFile);
+
+    return await ref.getDownloadURL();
+  }
 
   Future<void> addUser(UserModel user) async {
     await _firestore.collection('users').doc(user.id).set(user.toMap());
