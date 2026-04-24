7f7ebd20da872ef41c7676abf4b327ff91718e5d
hibabinth <hibabinth916@gmail.com>
2026-04-24 08:26:19 -0400

    Configure Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'presentation/viewmodels/auth_viewmodel.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,9 +15,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text("Firebase Connected"))),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => AuthViewModel())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'TOTAL-X',
+        theme: ThemeData(useMaterial3: true, fontFamily: 'Roboto'),
+        home: const Scaffold(body: Center(child: Text('Provider Connected'))),
+      ),
     );
   }
 }
